7e3f73865d7b2f81b62f88805667b3c813837a31
Nozara Sundus <142619439+nzs0111@users.noreply.github.com>
2023-11-26 22:46:51 -0600

    Finalized coding for project
    This coding is modified. It's finalized version from my side.

--- coding for project.R
+++ coding for project.R
@@ -1,145 +1,172 @@
-# Load necessary libraries
+#Load libraries
 library(ggplot2)
 library(reshape2)
-library(tidyr)
 
-# Set seed for reproducibility
-set.seed(123)
-
-# Generate data
-X <- matrix(rnorm(600), nrow = 200) # matrix of predictors
-X <- cbind(rep(1, 200), X) # add a column of ones for the intercept
-beta <- c(-1, 4, -5, 2) # true coefficient values
-linear_pred <- X %*% beta
-prob <- 1 / (1 + exp(-linear_pred)) # convert to probabilities
-y <- rbinom(200, size = 1, prob = prob) # generate binary outcomes
-
-# Scale the predictors (excluding the intercept)
-X_scaled <- scale(X[, -1])
-X_scaled <- cbind(X[, 1], X_scaled) # add the intercept back
-
-# Logistic Regression Function with Bootstrap Confidence Intervals
-logistic_regression_with_bootstrap <- function(X, y, alpha = 0.05, n_bootstrap = 20) {
-  logistic_loss <- function(beta, X, y) {
-    p <- 1 / (1 + exp(-X %*% beta))
+# Define the logistic regression function
+logistic_regression <- function(X, y) {
+  # Logistic function
+  logistic_function <- function(beta, X) {
+    1 / (1 + exp(-X %*% beta))
+  }
+  
+  # Loss function for logistic regression
+  loss_function <- function(beta, X, y) {
+    p <- logistic_function(beta, X)
     -sum(y * log(p) + (1 - y) * log(1 - p))
   }
   
-  init_beta <- rep(0, ncol(X))
-  optim_result <- optim(par = init_beta, fn = logistic_loss, X = X, y = y, method = "Nelder-Mead", control = list(maxit = 1000))
-  estimated_beta <- optim_result$par
+  # Initial estimate using least squares
+  initial_beta <- solve(t(X) %*% X) %*% t(X) %*% y
   
-  bootstrap_betas <- matrix(NA, nrow = n_bootstrap, ncol = length(estimated_beta))
-  set.seed(123) # For reproducibility
-  for (i in 1:n_bootstrap) {
-    indices <- sample(1:nrow(X), replace = TRUE)
-    X_boot <- X[indices, ]
-    y_boot <- y[indices]
-    optim_result_boot <- optim(par = init_beta, fn = logistic_loss, X = X_boot, y = y_boot, method = "Nelder-Mead", control = list(maxit = 1000))
-    bootstrap_betas[i, ] <- optim_result_boot$par
-  }
-  
-  ci_lower <- apply(bootstrap_betas, 2, function(beta) quantile(beta, alpha/2))
-  ci_upper <- apply(bootstrap_betas, 2, function(beta) quantile(beta, 1 - alpha/2))
-  
-  list(
-    estimated_beta = estimated_beta,
-    bootstrap_betas = bootstrap_betas,
-    confidence_intervals = list(lower = ci_lower, upper = ci_upper)
-  )
+  # Optimization
+  optim_result <- optim(initial_beta, fn = loss_function, X = X, y = y, method = "BFGS")
+  return(optim_result$par)
 }
 
-# Run the Logistic Regression with Bootstrap
-result <- logistic_regression_with_bootstrap(X_scaled, y, alpha = 0.05, n_bootstrap = 20)
+# Define the bootstrap confidence interval function
+bootstrap_CI <- function(X, y, alpha = 0.05, n_bootstrap = 20) {
+  # Store bootstrap samples of coefficients
+  boot_coefs <- matrix(NA, nrow = n_bootstrap, ncol = ncol(X))
+  
+  for (i in 1:n_bootstrap) {
+    # Sample with replacement
+    sample_indices <- sample(1:nrow(X), replace = TRUE)
+    X_boot <- X[sample_indices, ]
+    y_boot <- y[sample_indices]
+    
+    # Estimate coefficients for this bootstrap sample
+    boot_coefs[i, ] <- logistic_regression(X_boot, y_boot)
+  }
+  
+  # Calculate confidence intervals
+  CI_lower <- apply(boot_coefs, 2, function(coef) quantile(coef, alpha / 2))
+  CI_upper <- apply(boot_coefs, 2, function(coef) quantile(coef, 1 - alpha / 2))
+  
+  # Return a matrix with lower and upper bounds
+  CI <- rbind(CI_lower, CI_upper)
+  rownames(CI) <- c("Lower", "Upper")
+  return(CI)
+}
 
-# Predicted Probabilities
-predict_logistic <- function(X, beta) {
+# Function to plot the fitted logistic curve
+plot_logistic_curve <- function(X, y, beta_hat) {
+  df <- data.frame(X = X[, 2], y = y) # Assuming second column of X is the predictor
+  ggplot(df, aes(x = X, y = y)) +
+    geom_point() +
+    stat_smooth(method = "glm", method.args = list(family = "binomial"), se = FALSE, color = "blue") +
+    labs(title = "Logistic Regression Curve", x = "Predictor", y = "Response")
+}
+
+# Function to calculate confusion matrix and related metrics
+calculate_metrics <- function(actual, predicted_probabilities, cutoff = 0.5) {
+  predicted <- ifelse(predicted_probabilities > cutoff, 1, 0)
+  TP <- sum((predicted == 1) & (actual == 1))
+  TN <- sum((predicted == 0) & (actual == 0))
+  FP <- sum((predicted == 1) & (actual == 0))
+  FN <- sum((predicted == 0) & (actual == 1))
+  
+  prevalence <- mean(actual)
+  accuracy <- (TP + TN) / (TP + FP + FN + TN)
+  sensitivity <- TP / (TP + FN) # Also known as recall or true positive rate
+  specificity <- TN / (TN + FP) # True negative rate
+  false_discovery_rate <- FP / (FP + TP)
+  diagnostic_odds_ratio <- (TP / FP) / (FN / TN)
+  
+  metrics <- list(
+    confusion_matrix = matrix(c(TP, FN, FP, TN), nrow = 2, byrow = TRUE, dimnames = list("Actual Positive" = c("Predicted Positive", "Predicted Negative"), "Actual Negative" = c("Predicted Positive", "Predicted Negative"))),
+    prevalence = prevalence,
+    accuracy = accuracy,
+    sensitivity = sensitivity,
+    specificity = specificity,
+    false_discovery_rate = false_discovery_rate,
+    diagnostic_odds_ratio = diagnostic_odds_ratio
+  )
+  
+  return(metrics)
+}
+
+# Function to plot metrics over various cutoff values
+plot_metrics_over_cutoff <- function(y, predicted_probabilities) {
+  cutoffs <- seq(0.1, 0.9, by = 0.1)
+  metrics_data <- sapply(cutoffs, function(cutoff) {
+    metrics <- calculate_metrics(y, predicted_probabilities, cutoff)
+    c(metrics$accuracy, metrics$sensitivity, metrics$specificity)
+  })
+  
+  # Convert the metrics data to a data frame for plotting
+  metrics_df <- as.data.frame(t(metrics_data))
+  names(metrics_df) <- c("Accuracy", "Sensitivity", "Specificity")
+  metrics_df$Cutoff <- cutoffs
+  
+  # Plot using ggplot2
+  long_metrics_df <- reshape2::melt(metrics_df, id.vars = "Cutoff")
+  ggplot(long_metrics_df, aes(x = Cutoff, y = value, colour = variable)) +
+    geom_line() +
+    labs(title = "Performance Metrics over Various Cutoffs", x = "Cutoff Value", y = "Metric Value") +
+    theme_minimal()
+}
+
+
+
+#Example
+
+set.seed(123) # For reproducibility
+
+# Generate predictors
+n <- 2000 # Number of observations
+p <- 3   # Number of predictors
+X <- matrix(rnorm(n * p), nrow = n)
+
+# Add an intercept
+X <- cbind(rep(1, n), X)
+
+# Define true coefficients (including intercept)
+beta_true <- c(-1, 2, -3, 1)
+
+# Compute logistic probabilities
+logit_probs <- 1 / (1 + exp(-X %*% beta_true))
+
+# Generate binary response variable
+y <- rbinom(n, size = 1, prob = logit_probs)
+
+# Check the first few rows of X and y
+head(X)
+head(y)
+
+# Logistic regression to estimate coefficients
+estimated_beta <- logistic_regression(X, y)
+print(estimated_beta)
+# Calculate bootstrap confidence intervals
+CI <- bootstrap_CI(X, y, alpha = 0.05, n_bootstrap = 20)
+print(CI)
+# Plotting the curve
+plot_logistic_curve(X, y, estimated_beta)
+
+# Logistic function using the estimated coefficients
+logistic_function <- function(beta, X) {
   1 / (1 + exp(-X %*% beta))
 }
-predicted_probs <- predict_logistic(X_scaled, result$estimated_beta)
 
-# Plotting the Fitted Logistic Curve
-plot_data <- data.frame(
-  Response = y,
-  FittedProbability = predicted_probs,
-  LinearPredictor = X_scaled %*% result$estimated_beta
-)
+# Calculate the predicted probabilities using the estimated coefficients
+predicted_prob <- logistic_function(estimated_beta, X)
 
-ggplot(plot_data, aes(x = LinearPredictor, y = FittedProbability)) +
-  geom_point(aes(color = as.factor(Response)), alpha = 0.6) +
-  geom_line(aes(group = 1), color = 'blue') +
-  scale_color_manual(values = c('red', 'green'), labels = c('0', '1')) +
-  labs(title = 'Fitted Logistic Curve', x = expression(X %*% hat(beta)), y = 'Predicted Probability') +
-  theme_minimal()
+# Calculate metrics using a cutoff of 0.5
+metrics <- calculate_metrics(y, predicted_prob, cutoff = 0.5)
+print(metrics$confusion_matrix)
+print(paste("Prevalence:", metrics$prevalence))
+print(paste("Accuracy:", metrics$accuracy))
+print(paste("Sensitivity:", metrics$sensitivity))
+print(paste("Specificity:", metrics$specificity))
+print(paste("False Discovery Rate:", metrics$false_discovery_rate))
+print(paste("Diagnostic Odds Ratio:", metrics$diagnostic_odds_ratio))
 
-# [Previous code for data generation, logistic regression function, and plotting...]
-
-# Metrics Across Cutoffs
-calculate_metrics <- function(conf_matrix) {
-  TP <- conf_matrix[2, 2]
-  FP <- conf_matrix[2, 1]
-  TN <- conf_matrix[1, 1]
-  FN <- conf_matrix[1, 2]
-  
-  Accuracy <- (TP + TN) / sum(conf_matrix)
-  Sensitivity <- TP / (TP + FN)
-  Specificity <- TN / (TN + FP)
-  Prevalence <- (TP + FN) / sum(conf_matrix)
-  FDR <- FP / (FP + TP)
-  DOR <- ifelse(FP == 0 | FN == 0, NA, (TP / FP) / (FN / TN))
-  
-  return(list(Accuracy = Accuracy, Sensitivity = Sensitivity, Specificity = Specificity, 
-              Prevalence = Prevalence, FDR = FDR, DOR = DOR))
-}
-
-evaluate_metrics_across_cutoffs <- function(X, y, predicted_probs) {
-  cutoffs <- seq(0.1, 0.9, by = 0.1)
-  metrics_list <- list()
-  
-  for (cutoff in cutoffs) {
-    predicted_classes <- ifelse(predicted_probs > cutoff, 1, 0)
-    conf_matrix <- table(Predicted = predicted_classes, Actual = y)
-    metrics_list[[as.character(cutoff)]] <- calculate_metrics(conf_matrix)
-  }
-  
-  return(metrics_list)
-}
-
-metrics_across_cutoffs <- evaluate_metrics_across_cutoffs(X_scaled, y, predicted_probs)
-
-# Convert list to data frame for plotting
-metrics_df <- do.call(rbind, metrics_across_cutoffs)
-metrics_df <- as.data.frame(metrics_df)
-colnames(metrics_df) <- c("Accuracy", "Sensitivity", "Specificity", "Prevalence", "FDR", "DOR")
-metrics_df$Cutoff <- as.numeric(rownames(metrics_df))
-
-# Reshaping the data frame for ggplot
-metrics_long <- pivot_longer(metrics_df, cols = -Cutoff, names_to = "Metric", values_to = "Value")
-
-# Ensure Cutoff and Value are numeric
-metrics_long$Cutoff <- as.numeric(metrics_long$Cutoff)
-metrics_long$Value <- as.numeric(metrics_long$Value)
-
-# Plotting the Metrics Across Cutoffs
-ggplot(metrics_long, aes(x = Cutoff, y = Value, color = Metric)) +
-  geom_line() +
-  labs(title = "Metrics Evaluated Over Different Cutoffs",
-       x = "Cutoff Value",
-       y = "Metric Value") +
-  theme_minimal() +
-  scale_color_brewer(palette = "Set1") +
-  guides(color = guide_legend(title = "Metrics"))
-
-# [Previous code for data generation, logistic regression function, plotting, and metric evaluation...]
-
-# Display Metrics for Each Cutoff
-for (cutoff in seq(0.1, 0.9, by = 0.1)) {
-  cat("Metrics for cutoff", cutoff, ":\n")
-  print(metrics_across_cutoffs[[as.character(cutoff)]])
-  cat("\n")
-}
+# Plotting the metrics over various cutoff values
+plot_metrics_over_cutoff(y, predicted_prob)
 
 
+# Calculate metrics using a cutoff of 0.5
+metrics <- calculate_metrics(y, predicted_prob, cutoff = 0.5)
+print(metrics)
 
-# Ref: https://chat.openai.com/share/6282c9b6-5ee4-4cbc-8f6e-08a5c9d01034
-  
+#Ref: https://chat.openai.com/g/g-zZ3HN933F-r-code-helper/c/bbb01533-affe-48d5-8e44-6cb8138a4a50
+
